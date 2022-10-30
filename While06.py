8584b592506af7c65d22b963cc7f18cb222a915b
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-31 01:16:01 +0500

    6

--- While06.py
+++ While06.py
@@ -10,7 +10,7 @@ def main(s):
     ans = 0
     i = 0
     while i < len(s):
-        if s[i] == 'A' or s[i]=='a' or s[i]=='E' or s[i]=='e' or s[i]=='I' or s[i]=='i' or s[i]=='U' or s[i]=='u' or s[i]=='O' or s[i]=='o' or s[i]=="O'" or s[i]=="o'":
+        if s[i] == 'A' or s[i]=='a' or s[i]=='E' or s[i]=='e' or s[i]=='I' or s[i]=='i' or s[i]=='U' or s[i]=='u' or s[i]=='O' or s[i]=='o':
             ans += 0
         else:
             ans += 1
