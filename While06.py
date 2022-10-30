a479dac3268014ac89696033f2526b988413a819
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-31 00:16:35 +0500

    6

--- While06.py
+++ While06.py
@@ -7,3 +7,12 @@ def main(s):
     Returns:
         int: return answer
     """
+    ans = 0
+    i = 0
+    while i < len(s):
+        if s[i] == 'A' or s[i]=='a' or s[i]=='E' or s[i]=='e' or s[i]=='I' or s[i]=='i' or s[i]=='U' or s[i]=='u' or s[i]=='O' or s[i]=='o' or s[i]=="O'" or a[i]=="o'":
+            ans += 0
+        else:
+            ans += 1
+    i += 1
+    return ans
